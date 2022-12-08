c32a21aba6a69ce65d7956be320cbfbb5ea9f81f
umarbekuzbekov <umarbekuzbekov1611@gmail.com>
2022-12-08 18:15:35 +0200

    uyga vazifa traffic light edi bajarildi

---
+++ main.js
@@ -0,0 +1,25 @@
+let yashil = document.getElementById("yashil")
+let sariq=document.getElementById("sariq")
+let qizil=document.getElementById("qizil")
+
+
+setInterval(function () {yashil.style.backgroundColor='green'
+sariq.style.backgroundColor="white"
+qizil.style.backgroundColor="white"
+}, 3000);
+
+setInterval(function () {yashil.style.backgroundColor='green'
+sariq.style.backgroundColor="yellow"
+qizil.style.backgroundColor="white"
+}, 4000);
+
+setInterval(function () {yashil.style.backgroundColor='white'
+sariq.style.backgroundColor="white"
+qizil.style.backgroundColor="red"
+}, 5500);
+
+setInterval(function () {yashil.style.backgroundColor='white'
+sariq.style.backgroundColor="yellow"
+qizil.style.backgroundColor="red"
+}, 6500);
+
